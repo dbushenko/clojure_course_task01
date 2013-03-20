9108a2ac23b8ad8211745016c5bddbcc328e4eb8
dimhold <dimhold@gmail.com>
2013-03-20 19:04:14 +0300

    Add solution based on map, reduce

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -4,26 +4,14 @@
 
 
 (defn get-links []
-" 1) Find all elements containing {:class \"r\"}.
-
-Example:
-[:h3 {:class \"r\"} [:a {:shape \"rect\", :class \"l\",
-                         :href \"https://github.com/clojure/clojure\",
-                         :onmousedown \"return rwt(this,'','','','4','AFQjCNFlSngH8Q4cB8TMqb710dD6ZkDSJg','','0CFYQFjAD','','',event)\"}
-                     [:em {} \"clojure\"] \"/\" [:em {} \"clojure\"] \" · GitHub\"]]
-
-   2) Extract href from the element :a.
-
-The link from the example above is 'https://github.com/clojure/clojure'.
-
-  3) Return vector of all 10 links.
-
-Example: ['https://github.com/clojure/clojure', 'http://clojure.com/', . . .]
-"
-  (let [data (parse "clojure_google.html")]
-    nil))
+(let [data (vec (parse "clojure_google.html"))]
+(defn get-class-r [data accum]
+  (reduce #(if (coll? %2)
+                (if-not (nil? (some #{{:class "r"}} %2))
+                    [(get %2 2)]
+                    (reduce conj % (get-class-r %2 %))))
+          accum data))
+(map #(get-in % [1 :href]) (get-class-r data []))))
 
 (defn -main []
   (println (str "Found " (count (get-links)) " links!")))
-
-
