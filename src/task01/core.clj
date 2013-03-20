d184da5012e6b7bed5280677bb0e898c22609ece
dimhold <dimhold@gmail.com>
2013-03-20 19:07:56 +0300

    Add formatting

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -4,14 +4,14 @@
 
 
 (defn get-links []
-(let [data (vec (parse "clojure_google.html"))]
-(defn get-class-r [data accum]
-  (reduce #(if (coll? %2)
-                (if-not (nil? (some #{{:class "r"}} %2))
-                    [(get %2 2)]
-                    (reduce conj % (get-class-r %2 %))))
-          accum data))
-(map #(get-in % [1 :href]) (get-class-r data []))))
+  (let [data (vec (parse "clojure_google.html"))]
+    (defn get-class-content [data accum]
+      (reduce #(if (coll? %2)
+               (if-not (nil? (some #{{:class "r"}} %2))
+                  [(get %2 2)]
+                  (reduce conj % (get-class-content %2 %))))
+      accum data))
+    (map #(get-in % [1 :href]) (get-class-content data []))))
 
 (defn -main []
   (println (str "Found " (count (get-links)) " links!")))
