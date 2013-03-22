dae7e052023ba2ac0bc5c62ebe9fca3bf3c6cfa4
Alex Ott <alexott@gmail.com>
2013-03-22 12:50:02 +0100

    Update core.clj
    comments on solution

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -5,7 +5,12 @@
 
 (defn get-links []
   (let [data (vec (parse "clojure_google.html"))]
+;; defn определяет "top level" объект, так что лучше ее вынести за пределы функции 
+;; (get-links...)
+;; если же имеется необходимость определить функцию внутри другой функции, то можно воспользоваться (let [func (fn [...))
+;; или letfn.
     (defn get-class-content [data accum]
+;; при использовании reduce, для читабельности удобней пользоваться (fn [arg1 arg2] - тогда легче будет понимать код
       (reduce #(if (coll? %2)
                (if-not (nil? (some #{{:class "r"}} %2))
                   [(get %2 2)]
